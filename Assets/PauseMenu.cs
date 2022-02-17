8800854cb1245806797221cf91526e974edcce10
starowicz727 <starowicz727@gmail.com>
2022-02-17 14:03:03 +0100

    pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false; //moze sie przydac do mierzenia czasu moze tutaj bedziemy mierzyc czas? 
+    public GameObject pauseMenuUI;
+    public GameObject hudUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused) //kiedy klikamy esc ¿eby wyjœæ z pause menu
+            {
+                Resume();
+            }
+            else // kiedy klikamy esc ¿eby wyjœæ z gry
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        hudUI.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        hudUI.SetActive(false); //gdy tego nie zrobie, panel w hud blokuje dostêp do buttonow 
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true;
+    } 
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); //mo¿e lepiej nazwy sceny nie zapisywaæ dos³ownie tylko zmienn¹?
+    }
+}
