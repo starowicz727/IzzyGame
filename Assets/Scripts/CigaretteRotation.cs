1cdaf6b6c4f794c3b9fd2e511e7a9faafb9c4129
starowicz727 <starowicz727@gmail.com>
2022-02-17 21:07:51 +0100

    hit color

--- Assets/Scripts/CigaretteRotation.cs
+++ Assets/Scripts/CigaretteRotation.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CigaretteRotation : MonoBehaviour
 {
-    private float rotationsPerMinute = 20;
+    public Text livesCountTxt;
+    public GameObject HitColor;
+    private float rotationsPerMinute = 20f;
     void Update()
     {
         transform.Rotate(0, 0, 6.0f * rotationsPerMinute * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            HitColor.SetActive(true);
+            GameState.LivesNumber -= 1;
+            livesCountTxt.text = GameState.LivesNumber.ToString();
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            HitColor.SetActive(false);
+        }
     }
 }
